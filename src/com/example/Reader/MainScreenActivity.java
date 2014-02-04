3e848fa5e1a7fabe03e1e96e5629d3ca67382f6f
Kadir Anil Turgut <kanilturgut@gmail.com>
2014-02-04 23:21:04 +0200

    Initial commit

---
+++ src/com/example/Reader/MainScreenActivity.java
@@ -0,0 +1,110 @@
+package com.example.Reader;
+
+import android.app.Activity;
+import android.content.res.Configuration;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Kadir Anil Turgut
+ * Date: 02.02.2014
+ * Time: 23:03
+ */
+public class MainScreenActivity extends Activity {
+
+    private DrawerLayout mDrawerLayout = null;
+    private ActionBarDrawerToggle mDrawerToggle = null;
+    private RelativeLayout myRelativeDrawerLayout = null;
+
+    private ListView mDrawerList = null;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+        getActionBar().setTitle("TEPAV");
+        getActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#2b2933")));
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        myRelativeDrawerLayout = (RelativeLayout) findViewById(R.id.relativeDrawerLayout);
+        mDrawerList = (ListView) findViewById(R.id.listViewDrawer);
+
+        String[] TITLE = {"anil", "turgut", "adasds", "asdsad"};
+        mDrawerList.setAdapter(new ArrayAdapter<String>(MainScreenActivity.this, android.R.layout.simple_list_item_1,TITLE));
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                mDrawerLayout.closeDrawer(myRelativeDrawerLayout);
+            }
+        });
+
+        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                R.drawable.ic_drawer,
+                R.string.drawer_open,
+                R.string.drawer_close
+        ) {
+            @Override
+            public void onDrawerClosed(View view) {
+                invalidateOptionsMenu(); // creates call to
+                // onPrepareOptionsMenu()
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                invalidateOptionsMenu(); // creates call to
+                // onPrepareOptionsMenu()
+            }
+        };
+
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        // enable ActionBar app icon to behave as action to toggle nav drawer
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main_screen, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (mDrawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggls
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+}
---
+++ src/com/example/Reader/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.example.Reader;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+               startActivity(new Intent(SplashActivity.this, MainScreenActivity.class));
+            }
+        }, 3000);
+    }
+}
