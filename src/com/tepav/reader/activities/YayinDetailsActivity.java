f50360cf04efe360689f24e55d92791b58f871bc
Kadir Anil Turgut <kanilturgut@gmail.com>
2014-02-10 12:46:41 +0200

    pdf on yayindetailsactivity

--- src/com/tepav/reader/activities/YayinDetailsActivity.java
+++ src/com/tepav/reader/activities/YayinDetailsActivity.java
@@ -1,14 +1,19 @@
 package com.tepav.reader.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.webkit.WebView;
 import android.widget.TextView;
 import com.tepav.reader.R;
+import com.tepav.reader.models.File;
 import com.tepav.reader.models.Yayin;
+
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -17,15 +22,24 @@ import com.tepav.reader.models.Yayin;
  * Time: 21:44
  */
 public class YayinDetailsActivity extends Activity {
+
+    List<File> fileList = null;
+    Yayin yayin = null;
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_yayin_details);
 
-        Yayin yayin = (Yayin) getIntent().getSerializableExtra("class");
+        yayin = (Yayin) getIntent().getSerializableExtra("class");
 
         getActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#352354")));
         getActionBar().setTitle(yayin.getYtitle());
         getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //if some pdf files exist for this yayin
+        if (isFilesExist()) {
+            fileList = yayin.getFiles();
+        }
 
         TextView tvYayinDetailTitle = (TextView) findViewById(R.id.tvYayinDetailTitle);
         tvYayinDetailTitle.setText(yayin.getYtitle());
@@ -36,6 +50,11 @@ public class YayinDetailsActivity extends Activity {
 
         WebView mWebView = (WebView) findViewById(R.id.webview);
         mWebView.loadData(yayin.getYcontent(), "text/html; charset=UTF-8", null);
+
+        if (fileList != null) {
+            mWebView.getSettings().setJavaScriptEnabled(true);
+            mWebView.loadUrl("http://docs.google.com/gview?embedded=true&url=" + yayin.getFiles().get(0).getUrl());
+        }
     }
 
     @Override
@@ -50,5 +69,9 @@ public class YayinDetailsActivity extends Activity {
                 return super.onOptionsItemSelected(item);
         }
         return true;
+    }
+
+    private boolean isFilesExist() {
+        return (yayin.getFiles().size() > 0);
     }
 }
